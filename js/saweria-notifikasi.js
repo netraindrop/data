c7cd8060bf122f760cbafa91a999863bb3f09199
Ernestoyoofi <94425296+ernestoyoofi@users.noreply.github.com>
2023-03-19 10:46:31 +0700

    Update saweria-notifikasi.js
    Signed-off-by: Ernestoyoofi <94425296+ernestoyoofi@users.noreply.github.com>

--- js/saweria-notifikasi.js
+++ js/saweria-notifikasi.js
@@ -1,10 +1,15 @@
-const WebSocket = require("ws")                                                                                                                                                   const url = "wss://events.saweria.co/stream"                                             const apikey = "<your stream key>"                                                                                                                                 const socket = new WebSocket(`${url}?streamKey=${apikey}`,{
+const WebSocket = require("ws")
+const url = "wss://events.saweria.co/stream"
+const apikey = "<your stream key>"
+const socket = new WebSocket(`${url}?streamKey=${apikey}`,{
   headers: {
     Origin: "https://saweria.co",
     "User-agent": "Nodejs/16.1.2",
     "Cache-Control": "no-cache"
    }
-})                                                                                                                                                                                socket.on("message", (msg) => {
+})
+
+socket.on("message", (msg) => {
   const data = JSON.parse(msg.toString())
   if(data.type != "donation"){
     return ;
